eb3ef0e10272a4017ed6aab4f95b1d6c75db0d00
skuco <Str@toc@ster1>
2020-03-24 18:06:56 +0100

    finished first test

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -1,12 +1,13 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DemoTest {
     private WebDriver driver;
-    private final String BASE_URL = "http://localhost";
+    private final String BASE_URL = "http://localhost/registracia.php";
 
     @Before
     public void setUp(){
@@ -17,7 +18,13 @@ public class DemoTest {
     @Test
     public void test(){
         driver.get(BASE_URL);
-
+        driver.findElement(By.xpath("//input[@name='email']")).sendKeys("jozkorozko@gmail.com");
+        driver.findElement(By.xpath("//input[@name='name']")).sendKeys("Jozko");
+        driver.findElement(By.xpath("//input[@name='surname']")).sendKeys("Rozko");
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("P@ssword123");
+        driver.findElement(By.xpath("//input[@name='password-repeat']")).sendKeys("P@ssword123");
+        driver.findElement(By.xpath("//input[@id='checkbox']")).click();
+        driver.findElement(By.xpath("//button[@class='btn btn-success btn-lg btn-block']")).click();
     }
 
     @After

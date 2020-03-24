ba024ca300fec8ce59b74d9b12356cb601c19133
skuco <Str@toc@ster1>
2020-03-24 10:00:13 +0100

    initial commit with two selenium tests, added drivers

---
+++ src/test/java/DemoTest.java
@@ -0,0 +1,28 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DemoTest {
+    private WebDriver driver;
+    private String BASE_URL = "http://localhost";
+
+    @Before
+    public void setUp(){
+        System.setProperty("webdriver.gecko.driver","src/test/resources/drivers/geckodriver.exe");
+        driver = new FirefoxDriver();
+    }
+
+    @Test
+    public void test(){
+        driver.get(BASE_URL);
+
+    }
+
+    @After
+    public void tearDown(){
+
+    }
+
+}
---
+++ src/test/java/InitialTest.java
@@ -0,0 +1,26 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class InitialTest {
+    private WebDriver driver;
+    private final String BASE_URL = "http://localhost";
+
+    @Before
+    public void setUp(){
+        System.setProperty("webdriver.gecko.driver","src/test/resources/drivers/geckodriver.exe");
+        driver = new FirefoxDriver();
+    }
+
+    @Test
+    public void test(){
+        driver.get(BASE_URL);
+    }
+
+    @After
+    public void tearDown(){
+        driver.quit();
+    }
+}

33fda8fd5b2420fd7f2da403a933840bd9b59ee0
skuco <Str@toc@ster1>
2020-03-25 17:43:26 +0100

    added DRY parts to asserts

--- src/test/java/CountTest.java
+++ src/test/java/CountTest.java
@@ -15,17 +15,20 @@ public class CountTest {
         System.setProperty("webdriver.gecko.driver","src/test/resources/drivers/geckodriver.exe");
         driver = new FirefoxDriver();
         driver.get(BASE_URL);
+        driver.manage().window().maximize();
     }
 
     @Test
     public void test() {
 
         // Helping stuff
+        driver.findElement(By.id("clicks")).getText();
         System.out.print("Number of clicks: " + driver.findElement(By.id("clicks")).getText());
-        //driver.findElement(By.id("clicks")).getText();
-        //driver.findElement(By.id("clickMe")).click();
+        driver.findElement(By.id("clickMe")).click();
+        driver.navigate().refresh();
 
         // Assertion playground
+        // Wrong not-DRY principle
         Assert.assertEquals( "0", driver.findElement(By.id("clicks")).getText());
         driver.findElement(By.id("clickMe")).click();
         Assert.assertEquals( "1", driver.findElement(By.id("clicks")).getText());
@@ -37,11 +40,27 @@ public class CountTest {
         Assert.assertEquals( "4", driver.findElement(By.id("clicks")).getText());
         driver.findElement(By.id("clickMe")).click();
         Assert.assertEquals( "5", driver.findElement(By.id("clicks")).getText());
+        driver.navigate().refresh();
+
+        // Correct DRY principle
+        // Check final count of clicks only at the end
+        for (int i = 0; i < 10; i++) {
+            driver.findElement(By.id("clickMe")).click();
+        }
+        Assert.assertEquals( "10", driver.findElement(By.id("clicks")).getText());
+        driver.navigate().refresh();
+
+        // Check every click
+        for (int i = 1; i < 11; i++) {
+            driver.findElement(By.id("clickMe")).click();
+            Assert.assertEquals( String.valueOf(i), driver.findElement(By.id("clicks")).getText());
+        }
+        driver.navigate().refresh();
     }
 
     @After
     public void tearDown(){
-        //driver.quit();
+        driver.quit();
     }
 
 }

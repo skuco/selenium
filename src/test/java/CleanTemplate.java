04127ffc8fc2a1a50493f5b4508188f220705bbf
skuco <Str@toc@ster1>
2020-03-27 16:47:16 +0100

    added CleanTemplate test - one more time :)

--- src/test/java/CleanTemplate.java
+++ src/test/java/CleanTemplate.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class CountTest {
+public class CleanTemplate {
     private WebDriver driver;
     private final String BASE_URL = "http://localhost/clickmebaby.php";
 

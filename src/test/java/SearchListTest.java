3ae5dfe020c00abc8c2ebb247361cf8fda509793
skuco <Str@toc@ster1>
2020-03-28 20:02:47 +0100

    added List creation and search in List test

---
+++ src/test/java/SearchListTest.java
@@ -0,0 +1,37 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.List;
+
+public class SearchListTest {
+    private WebDriver driver;
+    private final String BASE_URL = "http://localhost/tabulka.php";
+
+    @Before
+    public void setUp(){
+        System.setProperty("webdriver.gecko.driver","src/test/resources/drivers/geckodriver.exe");
+        driver = new FirefoxDriver();
+        driver.get(BASE_URL);
+    }
+
+    @Test
+    public void test() {
+        List<WebElement> rows = driver.findElements(By.xpath("//table/tbody/tr"));
+        System.out.println(rows);
+        for (WebElement row : rows) {
+            System.out.println(row.findElement(By.xpath("./td[4]")).getText());
+            Assert.assertFalse(row.findElement(By.xpath("./td[4]")).getText().isEmpty());
+        }
+    }
+
+    @After
+    public void tearDown(){
+    }
+
+}

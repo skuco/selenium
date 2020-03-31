4c5f5ae13509cd3bd34181a38ebe0a485d1f650e
skuco <Str@toc@ster1>
2020-03-31 18:05:27 +0200

    added wait for element test

--- src/test/java/SpellingTest_ForIfElseTest.java
+++ src/test/java/SpellingTest_ForIfElseTest.java
@@ -20,20 +20,25 @@ public class SpellingTest_ForIfElseTest {
     @Test
     public void test(){
         // Initial Check
-        Assert.assertEquals("0" , driver.findElement(By.xpath("//h1[@id='clicks']")).getText());
-        Assert.assertEquals("klikov" , driver.findElement(By.xpath("//p[@class='description']")).getText());
+        Assert.assertEquals("0" , driver.findElement(By
+                .xpath("//h1[@id='clicks']")).getText());
+        Assert.assertEquals("klikov" , driver.findElement(By
+                .xpath("//p[@class='description']")).getText());
         // Spelling Check
         for (int i = 1; i < 11; i++) {
             driver.findElement(By.id("clickMe")).click();
             System.out.println(driver.findElement(By.xpath("//p[@class='description']")).getText());
             if (i==1){
-                Assert.assertEquals("klik" , driver.findElement(By.xpath("//p[@class='description']")).getText());
+                Assert.assertEquals("klik" , driver.findElement(By
+                        .xpath("//p[@class='description']")).getText());
             }
             if (i>=2 && i<=4){
-                Assert.assertEquals("kliky" , driver.findElement(By.xpath("//p[@class='description']")).getText());
+                Assert.assertEquals("kliky" , driver.findElement(By
+                        .xpath("//p[@class='description']")).getText());
             }
             if (i>=5){
-                Assert.assertEquals("klikov" , driver.findElement(By.xpath("//p[@class='description']")).getText());
+                Assert.assertEquals("klikov" , driver.findElement(By
+                        .xpath("//p[@class='description']")).getText());
             }
         }
     }
---
+++ src/test/java/WaitForElementTest.java
@@ -0,0 +1,46 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class WaitForElementTest {
+    private WebDriver driver;
+    private final String BASE_URL = "http://localhost";
+
+    @Before
+    public void setUp(){
+        System.setProperty("webdriver.gecko.driver","src/test/resources/drivers/geckodriver.exe");
+        driver = new FirefoxDriver();
+        driver.get(BASE_URL + "/zjavenie.php");
+    }
+
+    @After
+    public void tearDown(){
+        driver.quit();
+    }
+
+    @Test
+    public void test() throws InterruptedException {
+
+        // set static sleep value (very bad idea!)
+        driver.findElement(By.xpath("//button[@id='showHim']")).click();
+        Thread.sleep(2500);
+        driver.findElement(By.xpath("//div//img[@class='brano']"));
+        driver.navigate().refresh();
+
+        // set dynamic sleep / wait
+        driver.findElement(By.xpath("//button[@id='showHim']")).click();
+        new WebDriverWait(driver, 5)
+                .until(ExpectedConditions
+                        .visibilityOfElementLocated(By.xpath("//div//img[@class='brano']")));
+        driver.findElement(By.xpath("//div//img[@class='brano']"));
+    }
+
+
+
+}
